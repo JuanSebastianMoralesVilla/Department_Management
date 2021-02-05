58169f6412f917934e4b990293f6625e86973a2a
JuanSebastianMoralesVilla <juansebastianmoralesvilla@gmail.com>
2021-02-05 00:24:08 -0500

    Table view in c #

---
+++ Department_Management/Country.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Department_Management
+{
+   public  class Country
+    {
+        public String idDept { get; set; }
+        public String idMunicipio { get; set; }
+        public String NameDept { get; set; }
+        public String NameMunicipio { get; set; }
+        public String typeDept { get; set; }
+
+
+    }
+}
--- Department_Management/Form1.cs
+++ Department_Management/Form1.cs
@@ -12,9 +12,34 @@ namespace Department_Management
 {
     public partial class Form1 : Form
     {
+
+
+        public List<Country> Countries {get;set;}
+
+
         public Form1()
         {
+
+            Countries = GetCountries();
+
             InitializeComponent();
+        }
+
+        private List<Country> GetCountries()
+        {
+
+            // agregar el formato csv
+            var list = new List<Country>();
+            list.Add(new Country()
+            {
+                idDept = "1",
+                idMunicipio = "1",
+                NameDept = "Antioquia",
+                NameMunicipio = "Medellin",
+                typeDept = "Municipio"
+            });
+
+            return list;
         }
 
         OpenFileDialog file = new OpenFileDialog();
@@ -33,6 +58,34 @@ namespace Department_Management
             } 
 
 
+        }
+
+       
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // departamentos
+            var depts = this.Countries;
+
+            dataGridView1.DataSource = depts;
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex != -1)
+            {
+                MessageBox.Show(comboBox1.SelectedIndex.ToString());
+            }
+
+
+
+            if (comboBox1.SelectedIndex == 0)  { 
+            dataGridView1.Columns["idDept"].Visible =true;
+                dataGridView1.Columns["idMunicipio"].Visible = false;
+                dataGridView1.Columns["idMunicipio"].Visible = false;
+
+            }
+        
         }
     }
 }
